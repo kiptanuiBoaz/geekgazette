eb1b88c7c8e6256a45f634f7eb3b0fbc80543ce4
kiptanuiBoaz <boaserem022@gmail.com>
2023-03-08 21:50:02 +0300

    added graphic

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import "./hero.scss";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import blogImg from "../../assets/hero/blogIllustrator.png"
 
 export const Hero = () => {
   return (
-    <section className='hero'>
-     
-      <div className='hero-left'>
-        <h1 className='slogan'>Where geeks come to get a fix</h1>
-        <p className='tagline'> Lorem ipsum dolor sit amet,  elit, labore et dolore magna aliqua.</p>
-        <button><NavLink to="sign-up" className="sign-up">Start Reading</NavLink></button>
+    <>
+      <div className='line'>
+        <hr />
       </div>
 
-      <div className='hero-right'>
-        <p>right</p>
-      </div>
-    </section>
+      <section className='hero'>
+
+        <div className='hero-left'>
+          <h1 className='slogan'>Where geeks come to get a fix</h1>
+          <p className='tagline'> Lorem ipsum dolor sit amet,  elit, labore et dolore magna aliqua.</p>
+          <button><NavLink to="sign-up" className="sign-up">Start Reading</NavLink></button>
+        </div>
+
+        <div className='hero-right'>
+         <img src={blogImg}/>
+        </div>
+      </section>
+    </>
   )
 }
