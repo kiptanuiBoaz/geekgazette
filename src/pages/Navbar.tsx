be999f2b0f032617c0dd280417525d57804b1613
kiptanuiBoaz <boaserem022@gmail.com>
2023-03-12 11:45:24 +0300

    added hover styles

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -18,17 +18,17 @@ export const Navbar = () => {
   const activeStyle = {
     color: "#4D7E3E",
     fontWeight: "600",
-    textDecoration: "underline",
-    textUnderlineThickness: "3px",
+    // textDecoration: "underline",
+    // textUnderlineThickness: "3px",
     display: "inline-block",
     position: "relative",
-    "&:after": {
-      content: "",
+    "&::after": {
+      content: "''",
       position: "absolute",
       width: "100%",
       height: "2px",
-      bottom: "0",
-      left: "0",
+      bottom: 0,
+      left: 0,
       backgroundColor: "#4D7E3E",
 
     }
