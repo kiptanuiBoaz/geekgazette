4b10abb4cbd815c54c91979d34b81044339eaa19
kiptanuiBoaz <boaserem022@gmail.com>
2023-03-21 13:48:30 +0300

    styled login form

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import logo from "../assets/navbar/logo-no-bg-green.png";
-import "./login-form.scss"
+import "./login-form.scss";
+import { Link } from "react-router-dom";
+import { BiHide, BiShow } from "react-icons/bi"
 
 export const LoginForm = () => {
+    const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false);
     return (
         <section className='login-container'>
+            <div className="container-left"></div>
+            <div className="container-right"></div>
+
             <div className="form-container">
                 <img className="logo" src={logo} alt="logo" />
 
@@ -17,11 +24,29 @@ export const LoginForm = () => {
                     <br />
                     <input
                         className="password"
-                        type="password"
+                        type= {passwordVisibility ? "password" : "text"}
                         placeholder="Enter your password"
+                        autoComplete="off"
                     />
+
+                    <div
+                        className="toggle-password-visibility"
+                        onClick={() => setPasswordVisibility(!passwordVisibility)}
+                    >
+                        {!passwordVisibility
+                            ? <div className="show">  <BiShow /> </div>
+                            : <div className="hide">  <BiHide /> </div>
+                        }
+                    </div>
+
+                    <button type="submit" className="login-button">Sign In</button>
+                    
                 </form>
+                <div className="sign-up-link">
+                        <p className="sign-up-text">New here? Go to <Link to="/sign-up">Sign-up</Link></p>
+                    </div>
             </div>
+
 
 
         </section>
