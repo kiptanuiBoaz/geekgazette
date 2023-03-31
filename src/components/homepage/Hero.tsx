6ba879de361d46f632581b23ddc63b8ae466d868
kiptanuiBoaz <boaserem022@gmail.com>
2023-03-31 11:29:30 +0300

    added image preview

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -3,54 +3,12 @@ import "./hero.scss";
 import { NavLink } from "react-router-dom";
 import blogImg from "../../assets/hero/illustrator.png";
 import { categoryList as categories } from "../../assets/read/categories";
-import styled, { keyframes } from 'styled-components';
 
-const moveDown = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(-50%);
-  }
-  50% {
-    opacity: 1;
-    transform: translateY(0%);
-  }
-  100% {
-    opacity: 0;
-    transform: translateY(50%);
-  }
-`;
-
-const CategoryContainer = styled.p`
-  position: relative;
-  height: 50px;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-
-`;
-
-const CategoryText = styled.span<{ animate: boolean }>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
-  text-align: center;
-  font-size: 20px;
-  font-weight: bold;
-  color: #333;
-  animation-name: ${({ animate }) => animate ? moveDown : 'none'};
-  animation-duration: 2.5s;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-`;
 
 
 export const Hero = () => {
   const [displayHero, setDisplayHero] = useState<boolean>(true);
   const [categoryIndex, setCategoryIndex] = useState(0);
-  const [category, setCategory] = useState<string>("Sports");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -70,18 +28,11 @@ export const Hero = () => {
         <div className='hero-left'>
           <h1 className='slogan'>Where geeks come to get a fix</h1>
 
-{/* 
-          <CategoryContainer> */}
-            <p className='tagline'>
-              Catch up with the lates on
-              {/* <CategoryText animate={true}> */}
-                <span> {categories[categoryIndex]}</span>
-              {/* </CategoryText> */}
-              and get to share with like-minded geeks.
-            </p>
-          {/* </CategoryContainer> */}
-
-
+          <p className='tagline'>
+            Catch up with the lates on
+            <span> {categories[categoryIndex]}</span>
+            and get to share with like-minded geeks.
+          </p>
 
           <button onClick={() => setDisplayHero(false)}>
             <NavLink to="/" className="sign-up">
--- src/pages/UserProfileForm.tsx
+++ src/pages/UserProfileForm.tsx
@@ -1,42 +1,52 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import "./user-profile-form.scss";
 import { FiEdit } from "react-icons/fi";
 import heroImage from "../assets/hero/illustrator.png";
 
 export const UserProfileForm = () => {
-
+    const [image, setImage] = useState<File | null>(null);
+    const [previewUrl, setPreviewUrl] = useState<string | null>(null);
     const fnameRef = useRef<HTMLInputElement>(null);
-
     useEffect(() => { fnameRef.current?.focus(); }, [])
 
+    console.log(image);
     return (
         <section className='profile-form-container'>
             <form className='profile-form'>
-            <p  className='instruction'>Please fill in your info before proceeding</p>
+                <p className='instruction'>Please fill in your info before proceeding</p>
                 <label htmlFor="file-upload" className='custom-file-upload'>
-               
-                    <img className='file-upload-image' src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541" />
+
+                    <img
+                        className='file-upload-image'
+                        src={previewUrl ?? "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"}
+                    />
                     <span className='prompt'> <FiEdit /> {' '}{" "}Upload Image </span>
                 </label>
 
                 <input
-                    ref={fnameRef}
                     id="file-upload"
                     className='image-input'
                     type="file"
                     accept="image/*"
+                    onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => {
+                            const selectedFile = e.target.files?.[0];
+                            if (selectedFile) {
+                                setImage(selectedFile);
+                                setPreviewUrl( URL.createObjectURL(selectedFile));
+                            }
+                        }
+                    }
 
                 />
                 <br />
 
-                <input className='fname' type='text' placeholder="First Name" />
+                <input ref={fnameRef} className='fname' type='text' placeholder="First Name" />
                 <input className='lname' type='text' placeholder="Last Name" />
                 <br />
 
                 <input className='headline' placeholder='Headline e.g Software Developer' />
                 <br />
-
-
 
                 <select className='gender-select' required>
                     <option value="">Selcet your gender</option>
