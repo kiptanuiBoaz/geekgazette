828463f367c65212006cbc57b6ad97d5fa4825b7
kiptanuiBoaz <boaserem022@gmail.com>
2023-05-25 04:33:21 +0300

    removed react node

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import "./spinner.scss";
+import React, { ReactNode } from 'react';
+import './spinner.scss';
 import { Loading } from 'notiflix';
 
-export const Spinner = () => {  return Loading.dots()}
+export const Spinner = () => {
+  return (
+    <section className='spinner'>
+      <div>{Loading.dots() as ReactNode}</div>
+    </section>
+  );
+};
