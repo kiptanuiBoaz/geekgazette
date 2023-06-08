1628483c3765367cd4c4f51a7573cbac684fb513
Boaz <boaserem022@gmail.com>
2023-06-08 09:01:42 +0300

    solved dot env issue

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
 
+console.log(import.meta.env)
+// console.log(process.env)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain:import.meta.env.REACT_APP_AUTH_DOMAIN,
-  projectId:import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket:import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain:import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:import.meta.env.VITE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
