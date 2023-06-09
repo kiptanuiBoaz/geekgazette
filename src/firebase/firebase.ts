38bf8e84277577d4a5fab1ecb989c2b1e48b694b
Boaz <boaserem022@gmail.com>
2023-06-09 11:21:44 +0300

    removed log statements

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
 
-console.log(import.meta.env)
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
